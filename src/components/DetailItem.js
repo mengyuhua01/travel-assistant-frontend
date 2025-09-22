97a739b9e369a113388f8440b5ff0a458992b9e1
SINJE2 <SINJE2@OOCL.COM>
2025-09-22 17:00:35 +0800

    feat: change ui

--- src/components/DetailItem.js
+++ src/components/DetailItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './DetailItem.css';
 
+// Added an `icon` prop to display an icon on the left of each detail item
 const DetailItem = ({ label, content, icon }) => {
   return (
     <div className="detail-item-container">
-      <div className="detail-item-icon">{icon}</div>
+      {icon && <div className="detail-item-icon">{icon}</div>}
       <div className="detail-item-content">
-        <h5 className="detail-item-label">{label}</h5>
+        {label && <h5 className="detail-item-label">{label}</h5>}
         <p className="detail-item-description">{content}</p>
       </div>
     </div>
