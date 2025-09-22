e7fddaacb4ec1b7d13eee5f89b66491e639a7836
SINJE2 <SINJE2@OOCL.COM>
2025-09-22 17:08:09 +0800

    feat: fix ui

--- src/components/DayDetails.js
+++ src/components/DayDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './DayDetails.css';
 import DetailItem from './DetailItem';
+import { FaSun, FaCoffee, FaCloudSun, FaUtensils, FaMoon, FaWineGlassAlt, FaBed, FaBus } from 'react-icons/fa';
 
 const DayDetails = ({ dayData }) => {
   return (
@@ -9,30 +10,30 @@ const DayDetails = ({ dayData }) => {
 
       <div className="session">
         <h3 className="session-title">Morning</h3>
-        <DetailItem label="Activities" content={dayData.morning} />
-        <DetailItem label="Breakfast" content={dayData.meals.breakfast} />
+        <DetailItem label="Activities" content={dayData.morning} icon={<FaSun />} />
+        <DetailItem label="Breakfast" content={dayData.meals.breakfast} icon={<FaCoffee />} />
       </div>
 
       <div className="session">
         <h3 className="session-title">Afternoon</h3>
-        <DetailItem label="Activities" content={dayData.afternoon} />
-        <DetailItem label="Lunch" content={dayData.meals.lunch} />
+        <DetailItem label="Activities" content={dayData.afternoon} icon={<FaCloudSun />} />
+        <DetailItem label="Lunch" content={dayData.meals.lunch} icon={<FaUtensils />} />
       </div>
 
       <div className="session">
         <h3 className="session-title">Evening</h3>
-        <DetailItem label="Activities" content={dayData.evening} />
-        <DetailItem label="Dinner" content={dayData.meals.dinner} />
+        <DetailItem label="Activities" content={dayData.evening} icon={<FaMoon />} />
+        <DetailItem label="Dinner" content={dayData.meals.dinner} icon={<FaWineGlassAlt />} />
       </div>
 
       <div className="session">
         <h3 className="session-title">Accommodation</h3>
-        <DetailItem label="Accommodation" content={dayData.accommodation} />
+        <DetailItem content={dayData.accommodation} icon={<FaBed />} />
       </div>
 
       <div className="session">
         <h3 className="session-title">Transportation</h3>
-        <DetailItem label="Transportation" content={dayData.transportation.details} />
+        <DetailItem content={dayData.transportation.details} icon={<FaBus />} />
       </div>
 
       <h3 className="daily-cost">Daily Cost: {dayData.dailyCost} 元</h3>
